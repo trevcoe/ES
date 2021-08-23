f648894268f542f13c072f2fe107a646095fb435
trevcoe <88930059+trevcoe@users.noreply.github.com>
2021-08-23 18:25:50 -0400

    Add files via upload

---
+++ ES5 constraints.js
@@ -0,0 +1,11 @@
+const PI = 3.14
+
+PI = 42
+
+var can be redeclared, let cannot. 
+
+var can be changed, const cannot.
+
+let can be reassigned, const cannot.
+
+hoisting is the way java script will run variables first
